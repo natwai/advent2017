0c86c76d9eaa0688091d52e9122f8c72162e66f2
Natasha <z5084631@zmail.unsw.edu.au>
2017-12-15 14:40:24 +1100

    advent02p2 complete

--- advent02p1.cpp
+++ advent02p1.cpp
@@ -2,21 +2,29 @@
 #include <algorithm>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
 
 
 int main()
 {
 	int min, max, sum = 0;
 	std::string in;
+	std::vector<int> nums;
 	std::ifstream myfile("input.txt");
 	if(myfile.is_open())
 	{
 		while(getline(myfile, in))
 		{
-			in.erase(std::remove_if(in.begin(), in.end(), isspace), in.end()); //removes spaces in input
-			min = *std::min_element(in.begin(), in.end()) - '0';
-			max = *std::max_element(in.begin(), in.end()) - '0';
+			std::istringstream iss(in); //for operating on the string... i need the iterator
+			std::copy(std::istream_iterator<int>(iss), std::istream_iterator<int>(), std::back_inserter(nums)); //somehow im treating the strings as ints
+			min = *std::min_element(nums.begin(), nums.end());
+			//std::cout << "min: " << min << std::endl;
+			max = *std::max_element(nums.begin(), nums.end());
+			//std::cout << "max: " << max << std::endl;
 			sum += max - min;
+			nums.clear();
 		}
 		std::cout << "sum: " << sum << std::endl;
 	}

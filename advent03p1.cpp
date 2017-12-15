b5d50c410ccccd82c01f0524960e420eaea380ea
Natasha <z5084631@zmail.unsw.edu.au>
2017-12-15 16:00:50 +1100

    added comments
    instructions for what im doing so i dont forget :>

--- advent03p1.cpp
+++ advent03p1.cpp
@@ -13,11 +13,13 @@ public:
 	int size;
 	int it = 1; //take mod 2 to check if we move counter
 	int ctr = 1; //increases everytime it%2 = 0
-	std::vector<int> = {R, U, L D}; //keep track of curr direction
+	std::vector<int> seq = {R, U, L D}; //keep track of curr direction
 
 	Graph(int s) : size{s} 
 	{
 		//construct the graph here
+		int val = 1; //will count up to size
+		//each inserted node will have a previous node to the right. a node will have a prev node in the down direction if curr direction is not down AND if val > 2.
 	} 
 
 	class Node {
